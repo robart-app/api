f76b8762d2b272a41ab11a12ed848a50bd5b3c3c
Juan Alvarado <jalvarado91@gmail.com>
2016-10-16 11:38:22 -0400

    enc1101

---
+++ templates.js
@@ -0,0 +1,46 @@
+const emotions = {
+  "The {{ noun }} exhibits a strong {{ adjective }}",
+  "The {{ adjective }} and {{ adjective }} Apollo and Daphne",
+  "There are trademark {{ adjective }} features contribute to a {{ adjective }} yet {{ adjective }} image:",
+  "{{ adjective }} viewpoint, strong {{ adjective }} of light and shadow, spacious {{ adjective }} patches.",
+  "Themes also recur — apparently {{ adjective }},  the principal subject always being the {{ adjective }}",
+  "Various compositional echoes (the {{ adjective }}, the {{ adjective }} entablature crossing the canvas allow to view this piece as a pastiche of such works",
+  "Envisaged as a part of a series, the painting stands out for its {{ adjective }}, almost brutal display of {{ adjective }}. In a curious overturn, the artist’s {{ adjective }} method has backfired:  it seems.",
+  "The sensation of an overwhelming, intimidating cliff, aided by the low vantage point, becomes even more intensified by the strong red colors.",
+  "It is harsh, uncompromising vision.",
+  "the ultimate source of {{ adjective }}",
+  "the {{ nouns }} face becomes an emotive center point: the {{ adjective }} discloses {{ adjective }}, the faint {{ adjective }} on {{ noun }} reveals a measure of pleasure from being noticed by the {{ noun }}, the downcast eyes express humility and inevitable doom. When combined, these emotions synthesize into a sense of fate, along with recognition and acceptance of it.",
+  "{{ noun }} absent-minded gaze points towards the {{ noun }} as if in an envy of the servant’s plain and uncomplicated position — the opposite of the {{noun}}.",
+  "The rest of lady’s body enacts mentioned feelings.",
+  "The artist in an unsure motion conveys which both repels and summons the {{ adjective }}.",
+  "The {{ noun }} rests weak-willed on the {{ adjective }}.",
+  "The {{ noun }} is a propelling tool, and this painting exemplifies how the {{ noun }} triggers the {{ adjective }}.",
+  "But I find the artist’s method of conveying the content of the piece even cleverer.",
+  "Without the need to go into the biblical details, it is easy to recognize the subject of the message as the {{ noun }} in front of us.",
+  "Though the sexiness of the luscious {{ adjective }} of the woman’s body may seem questionable by our contemporary standards.",
+  "Particularly the excessive {{ adjective }} demonstrates the artists ability to properly tell a {{ adjective }} story.",
+  "There are several attributes that substitute clothing as a telling sign of refined {{ adjective }}.",
+  "The elegant {{ noun }} and the {{ noun }} complete the picture.",
+  "This line assumes symbolic meaning.",
+  "In one way or another, all of these factors are involved in the (future) proceedings.",
+  "One possible interpretation would posit that the language used to write the {{ adjective }} is the metaphysical carrier of the knowledge unduly obtained from that same tree, thus linking the depicted event with the legendary {{ noun }}."
+};
+
+const descriptions = {
+  "The artist is a genre painter working with {{noun}}, which he applies on canvases or panels reaching between 10-20 inches (50 centimeters) in height and a similar range in width. The subject matter usually involves his {{ adjective }} place of mental residence, {{ noun }}.",
+  "The artist exhibits his work on his blog, as well as several local and online galleries. This review focuses on the artist’s depictions of different {{ nouns }} and {{ nouns }}.",
+  "The artist fauna paintings register psychological {{ adjective }} marked with {{ adjective }} and an underlying humorous streak. With the precision of an insightful observer, the artist recreates the subtle gestures of his subjects, capturing their essence and personality.",
+  "As a result, some of the pieces function as {{ adjective }}, representing, in general, the most {{ adjective }} character traits of that kind of animal.",
+  "Painting can be difficult due to the mercurial nature of the {{ noun }}: constantly on the move, feeding, flying, or courting; even the larger {[ noun ]}, when deliberately static, appear tense and ready to take wing.",
+  "The artist’s attention to the motivation of his subjects, combined with an understanding of their anatomy and unique locomotion, allows him to negotiate these complications, and render a convincing image that’s at once painterly and iconic.",
+  "A display of {{ adjective }}, where the {{ noun }} is shown exerting its entire mass to issue a message of warning, epitomizes the style. The {{ nouns }} 's {{ adjective }}, channeled via pictorial veracity, draws the attention and fascinates, as any good nature portrait should.",
+  "By picturing the models from such close proximity (as if suggesting a face-off between the audience and the subjects), the artist both cancels and emphasizes the differences between the human spectators and the {{ nouns }}. We are close to them, yet are so different. The incompatibility can be a rich ground for comic effect; the compatibility – a reflection on the unity of nature and the humans’ part in it.",
+  "Another common theme in the artist’s repertory is the {{ noun }}. A pronouncedly symmetrical animal, the crab can be compositionally challenging to paint; to avoid repetition and rigidity the painter positions the crabs askew, and adjusts the viewing angles to convey a sense of mass and presence.",
+  "Diagonals are not the only device employed to signal movement. Thick, rich, smeared strokes of vibrant color impart a sense of motion and progression through water and sand. The inconstant, varied texture of the brushwork, combined with the seemingly random scattering of color, becomes a powerful method of light representation. Where this quality peaks, the style of the paintings approximates impressionism.",
+  "The abundance of light – obvious to a painter living so close to a large body of water, a natural mirror and refractor of the sun’s rays – shows the environment at its busiest, liveliest moments. Using a brilliant white tone, Rick Nilson sprays light over everything, flooding the paintings with spontaneous, seemingly infinite energy. The animals materialize within that flux, restating life at its most immediate and invigorating.",
+  "The first thing I noticed when browsing through the works. In {{ nouns }} it was people against the walls; here, at sea, it is the {{ nouns }}. But perhaps the artist takes the contrast up a notch, to a level where it mutates into a real clash: that of the sea and the land and that of living on a constant move and in a permanent dwelling placed on the ground. Planes of bright single hue, which formed the walls in the town, stream down into the see and break down into shimmering reflections.",
+  "Appropriately, the viewing angle positions the observer just on the brink of the two {{ elements }}, maybe with an in"
+}
+
+export emotions;
+export descriptions;
